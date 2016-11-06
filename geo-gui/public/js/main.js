c72be7ee7caf68c0e3e3e07e4e6ef6f533dfdb47
capynet <capy.net@gmail.com>
2016-11-06 18:03:45 +0100

    Unify the funtion that calls backend results on the GUI

--- geo-gui/public/js/main.js
+++ geo-gui/public/js/main.js
@@ -16,82 +16,46 @@ $(function () {
     });
   }
 
+  /**
+   * Search on the backend.
+   * @param params
+   */
+  function backendSearch(params) {
+    console.log('Searching');
+    $.post('/' + engine + '/points/get', params)
+      .done(function (data) {
+        clearOverlays();
+        data.items.forEach(function (item) {
+          addMarker(item.coordinates.split(',')[0], item.coordinates.split(',')[1], $map.data('gmap').map);
+        });
+      });
+  }
+
   /* Km */
   var distance = 50;
-
   instanceMap('mapa', 'buscador', {zoom: 8, radius: distance});
-
-
   var $map = $('#mapa');
 
+  // Events
+  // -----------------------------------------------------
   $map.on('gmap.initialized', function (e, map, mapConf) {
-
-    $.post('/' + engine + '/points/get', {
-        lat: mapConf.initialLatLng[0],
-        lng: mapConf.initialLatLng[1],
-        distance: distance
-      })
-      .done(function (data) {
-        data.items.forEach(function (item) {
-          addMarker(item.coordinates.split(',')[0], item.coordinates.split(',')[1], map);
-        });
-
-      });
-
+    var params = {lat: mapConf.initialLatLng[0], lng: mapConf.initialLatLng[1], distance: distance};
+    backendSearch(params);
   });
 
   $map.on('marker.dragend', function (e, lat, lng) {
-
-    $.post('/' + engine + '/points/get', {
-        lat: lat,
-        lng: lng,
-        distance: Math.round($map.data('gmap').conf.distance || distance)
-      })
-      .done(function (data) {
-        clearOverlays();
-        data.items.forEach(function (item) {
-          addMarker(item.coordinates.split(',')[0], item.coordinates.split(',')[1], $map.data('gmap').map);
-        });
-
-      });
-
+    var params = {lat: lat, lng: lng, distance: Math.round($map.data('gmap').conf.distance || distance)};
+    backendSearch(params);
   });
 
   $map.on('circle.dragend', function (e, distance, lat, lng) {
-
-    $.post('/' + engine + '/points/get', {
-        lat: $map.data('gmap').conf.marker.getPosition().lat(),
-        lng: $map.data('gmap').conf.marker.getPosition().lng(),
-        distance: distance
-      })
-      .done(function (data) {
-        clearOverlays();
-
-        data.items.forEach(function (item) {
-          addMarker(item.coordinates.split(',')[0], item.coordinates.split(',')[1], $map.data('gmap').map);
-        });
-
-      });
-
-
+    var params = {lat: $map.data('gmap').conf.marker.getPosition().lat(), lng: $map.data('gmap').conf.marker.getPosition().lng(), distance: distance};
+    backendSearch(params);
   });
 
   $map.on('autocomplete.locationSelected', function (e, lat, lng) {
-
-    $.post('/' + engine + '/points/get', {
-        lat: lat,
-        lng: lng,
-        distance: Math.round($map.data('gmap').conf.distance || distance)
-      })
-      .done(function (data) {
-        clearOverlays();
-
-        data.items.forEach(function (item) {
-          addMarker(item.coordinates.split(',')[0], item.coordinates.split(',')[1], $map.data('gmap').map);
-        });
-
-      });
-
+    var params = {lat: lat, lng: lng, distance: Math.round($map.data('gmap').conf.distance || distance)};
+    backendSearch(params);
   });
 
   // We update the engine when the user changes it.
@@ -99,20 +63,8 @@ $(function () {
     engine = $(this).val();
     console.log("Selected " + engine + " engine");
 
-    $.post('/' + engine + '/points/get', {
-        lat: $map.data('gmap').conf.marker.getPosition().lat(),
-        lng: $map.data('gmap').conf.marker.getPosition().lng(),
-        distance: distance
-      })
-      .done(function (data) {
-        clearOverlays();
-
-        data.items.forEach(function (item) {
-          addMarker(item.coordinates.split(',')[0], item.coordinates.split(',')[1], $map.data('gmap').map);
-        });
-
-      });
-
+    var params = {lat: $map.data('gmap').conf.marker.getPosition().lat(), lng: $map.data('gmap').conf.marker.getPosition().lng(), distance: distance};
+    backendSearch(params);
   });
 
 });
